ce1941247313960dd9e6ce9c81bf242e02cecda3
Sam Robertson <robertsonnew@gmail.com>
2017-04-20 08:13:35 -0600

    second commit

---
+++ Angular2-GettingStarted-master/APM - Start/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>{{pageTitle}}</h1>
+`
+
+})
+
+export class AppComponent {
+    pageTitle: string = 'APM from component';
+}
